c30187197f641ce3a94fb00e447d476bd21b0b3f
Wth10 <weuberth10@gmail.com>
2022-10-16 17:10:43 -0300

    InitialCommit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <div className="containe text-center w-[300px] mt-[60px] m-auto">
+        <img
+          className="avatar"
+          src="https://github.com/Wth10.png"
+          alt="logotipo"
+        />
+        <h1 className="mt-[24px] mb-[8px] text-3xl">Wth10</h1>
+        <p className="">Wth@10</p>
+        <ul>
+          <li>
+            <a className="link_html" href="#" target="_blank">
+              📷 Instagram
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              📷 GitHub
+            </a>
+          </li>
+        </ul>
+        <footer>
+          <p>Welcome 🎉 Profile</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
